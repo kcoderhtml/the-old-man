43750e43aaf9330fb8e1488fe96ae6d8dfaacc1b
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2024-05-25 12:20:06 -0400

    feat: add clear function to clearn im

--- welcome.ts
+++ welcome.ts
@@ -7,3 +7,24 @@ export async function welcome(userID: string, client: WebClient) {
         text: "Welcome to Bag! I'm the old man who toddles",
     });
 }
+
+export async function clear(userID: string, client: WebClient) {
+    // delete all messages sent to the user
+    // list conversations and pick the one with the user
+    const conversations = await client.conversations.list({
+        types: "im",
+    });
+    console.log(conversations);
+    const conversation = conversations.channels?.find((channel) => channel.user === userID);
+    await client.conversations.history({
+        channel: conversation?.id as string,
+    }).then(async (res) => {
+        for (const message of res.messages as any[]) {
+            console.log(message);
+            await client.chat.delete({
+                channel: userID,
+                ts: message.ts as string,
+            });
+        }
+    });
+}
